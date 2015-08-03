40a535dfb79f0b036f5740d61f7acdcb8fca7434
WangMinjun <15895899337@163.com>
2015-08-03 09:23:10 +0800

    Initial Commit

---
+++ MJdeFM/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MJdeFM
+//
+//  Created by WangMinjun on 15/8/3.
+//  Copyright (c) 2015年 WangMinjun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
